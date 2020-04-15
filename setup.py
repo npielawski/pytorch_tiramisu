6cd5dbfa91837f9086ce22f3282cb5c61235e6b4
npielawski <nicolas@pielawski.fr>
2020-04-15 14:25:01 +0200

    Added file to subdirectory

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="pytorch_tiramisu",
+    version="1.0",
+    description="Pytorch Tiramisu Neural Network",
+    author="Nicolas Pielawski",
+    author_email="nicolas@pielawski.fr",
+    packages=["models"],
+    install_requires=[
+        "torch",
+    ],
+)
