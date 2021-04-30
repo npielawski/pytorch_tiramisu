22e3e2e5f3f8a2e30e3756c04685a045b67d1ce8
npielawski <npielawski@gmail.com>
2021-04-30 21:01:40 +0200

    Updated version in setup.py.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tiramisu",
-    version="1.0",
+    version="1.1",
     description="Better Tiramisu Neural Network",
     long_description=long_description,
     long_description_content_type="text/markdown",
